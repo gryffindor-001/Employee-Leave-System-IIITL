866726c9eb514ffe7ecd1904a134a954e4e3b235
gryffindor-001 <daivesh000@gmail.com>
2021-10-04 20:02:52 +0530

    Thought mmore features

--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const auth = require('../middleware/auth')
+const Leave = require('../models/leave')
 
 const router = new express.Router()
 
@@ -7,8 +8,13 @@ router.get('/user', auth, (req, res) => {
     res.render('home', {home: 2})
 })
 
-router.get('/user/me', auth, (req, res) => {
-    res.render('profile')
+router.get('/user/me', auth, async (req, res) => {
+    const user = req.user
+
+    const approved = await Leave.find({userID: req.user._id, status: "approve"})
+    const rejected = await Leave.find({userID: req.user._id, status: "reject"})
+
+    res.render('profile', {user, approved, rejected})
 })
 
 module.exports = router
